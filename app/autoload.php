260241d34c064ec7dbf508bf06aaae87516aab4a
Brent Shaffer <bshafs@gmail.com>
2015-10-15 11:47:52 -0700

    adds License information to all relevant filesgit diff

--- app/autoload.php
+++ app/autoload.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Symfony Standard Edition package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file at https://github.com/symfony/symfony-standard/blob/2.8/LICENSE
+ */
+
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
